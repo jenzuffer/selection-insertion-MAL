1e7e69c984dc46dcbe1333e763fccdaf655cf81d
Christian Moustesgård <cph-cm267@cphbusiness.dk>
2021-02-24 20:30:45 +0100

    sorting2 insertion implementation

--- src/main/java/dk/cphbusiness/mal2/Sorting2.java
+++ src/main/java/dk/cphbusiness/mal2/Sorting2.java
@@ -1,7 +1,6 @@
 package dk.cphbusiness.mal2;
 
 import dk.cphbusiness.mal.utils.Stopwatch;
-import kotlin.NotImplementedError;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,7 +22,7 @@ public class Sorting2 implements Algorithms {
     public List<String> selection() {
         FileInputStream inputStream = null;
         try {
-            inputStream = new FileInputStream(path1);
+            inputStream = new FileInputStream(path);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -75,8 +74,56 @@ public class Sorting2 implements Algorithms {
     }
 
     @Override
-    public List<String> selectionTakingAList(List<String> list) {
-        throw new NotImplementedError();
+    public List<String> selectionTakingAList(List<String> _unused) {
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Scanner sc = new Scanner(inputStream, "UTF-8");
+        List<String> stringList = new ArrayList<>();
+        try (Stopwatch sw = new Stopwatch()) {
+            while (sc.hasNextLine()) {
+                final String line = sc.nextLine();
+                final String[] split = line.split(delimiterPattern);
+                List<String> localStrings = new ArrayList<>();
+                for (String strSplit : split) {
+                    String s = strSplit.toLowerCase();
+                    s = s.replaceAll("\\p{Punct}", "");
+                    s = s.trim();
+                    if (s != null && !s.isEmpty()) {
+                        localStrings.add(s);
+                    }
+                }
+                int array_index = localStrings.size();
+                for (int i = 0; i < array_index; i++){
+                    String s = localStrings.get(i);
+                    int index_j = i - 1;
+                    while (index_j >= 0 && localStrings.get(index_j).compareTo(s) > 0){
+                        localStrings.set(index_j + 1, localStrings.get(index_j));
+                        index_j --;
+                    }
+                    localStrings.set(index_j + 1, s);
+                }
+
+                for (int i = 0; i < localStrings.size(); i++) {
+                    stringList.add(localStrings.get(i));
+                }
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("finished with sw time: ");
+        }
+        return new ArrayList<>();
     }
 
 
@@ -84,9 +131,11 @@ public class Sorting2 implements Algorithms {
         Sorting2 sorting2 = new Sorting2();
         List<String> selection = sorting2.selection();
         System.out.println("selection size: " + selection.size());
-        System.out.println(selection);
+        //System.out.println(selection);
         /*for (String str : selection) {
             System.out.println(str);
         } */
+        sorting2.selectionTakingAList(new ArrayList<>());
+        System.out.println("selection size: " + selection.size());
     }
 }
